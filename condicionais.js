8d947beb625bd394a47b6e9480640669196d43bc
Nawldo <nalddu@yahoo.com.br>
2025-05-30 11:10:04 -0300

    Exercicios início

---
+++ condicionais.js
@@ -0,0 +1,31 @@
+/* const idade = 17.9;
+
+if (idade >= 18) {
+    console.log('Entrada permitida.')
+} else {
+    console.log('Entrada não permitida.')
+} */
+
+
+
+/* const temperatura = 45;
+
+if (temperatura > 26)
+{
+    console.log('quente')
+} else if (temperatura >= 22 && temperatura <=26 ) {
+    console.log('Agradável')
+} else {
+    console.log('frio')
+} */
+
+
+const idade = 13;
+
+if (idade <= 12) {
+    console.log('Infantil')
+} else if (idade > 12 && idade < 18 ) {
+    console.log('Juvenil')
+} else {
+    console.log('Adulto')
+}
